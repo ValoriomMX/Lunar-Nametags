3800121dc59dfdbf813ad36ac2456cbc7249c6ce
ValoriomMX <76182918+ValoriomMX@users.noreply.github.com>
2023-02-13 16:12:28 -0800

    Create Color.java

---
+++ src/main/java/net/moonsworth/lunar/utils/Color.java
@@ -0,0 +1,17 @@
+package net.moonsworth.lunar.utils;
+
+import org.bukkit.ChatColor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Color {
+    
+    public static String translate(String in) {
+        return ChatColor.translateAlternateColorCodes('&', in);
+    }
+
+    public static List<String> translate(List<String> lines) {
+        return lines.stream().map(Color::translate).collect(Collectors.toList());
+    }
+}
